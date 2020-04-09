43df6f2afa9695abc8db7250891ed929501cb378
Neer619 <36510513+Neer619@users.noreply.github.com>
2020-04-09 21:54:38 +0530

    Add files via upload

--- includes/insertData.inc.php
+++ includes/insertData.inc.php
@@ -32,7 +32,7 @@ if($device=='mobile')
     
     // $_FILES['photo']['name']=md5($photo['name']).".".$fileExt;
 
-    $photo_location="images/mobile/".md5($photo['name']).".".$fileExt;
+    $photo_location="C:/xampp/htdocs/eComm_php/images/mobile/".md5($photo['name']).".".$fileExt;
 
 
 
@@ -77,7 +77,7 @@ else if($device=='laptop')
 
 
 
-    $sql="INSERT INTO mobile (photo,price,brand,model_name,model_number,ram,gpu,os,processor_type,hdd_space,battery) VALUES ( '$photo_location','$price', '$brand', '$model_name', '$model_num', '$ram', '$gpu', '$os', '$processor_type', '$hdd_space', '$battery') ;";
+    $sql="INSERT INTO laptop (photo,price,brand,model_name,model_num,ram,gpu,os,processor,hdd_space,battery) VALUES ( '$photo_location','$price', '$brand', '$model_name', '$model_num', '$ram', '$gpu', '$os', '$processor', '$hdd_space', '$battery') ;";
 
     if(mysqli_query($conn,$sql))
     {
@@ -112,53 +112,11 @@ else if($device=='camera')
     
     // $_FILES['photo']['name']=md5($photo['name']).".".$fileExt;
 
-    $photo_location="images/mobile/".md5($photo['name']).".".$fileExt;
+    $photo_location="images/camera/".md5($photo['name']).".".$fileExt;
 
 
 
-    $sql="INSERT INTO mobile (photo,price,brand,model_name,model_num,megapixel,sensor_type,type) VALUES ( '$photo_location','$price', '$brand', '$model_name', '$model_num', '$megapixel','$sensor_type','$type') ;";
-
-    if(mysqli_query($conn,$sql))
-    {
-        move_uploaded_file($_FILES['photo']['tmp_name'],$photo_location);
-
-        // header("Location: ../inserPage.php");
-
-        echo "Success";
-        exit();
-    }
-    else{
-        echo "Fail".mysqli_error($conn);
-    }
-
-
-
-}
-else if($device=='mobile')
-{
-    $photo=$_FILES['photo'];
-    $price=$_POST['price'];
-    $brand=$_POST['brand'];
-    $model_name=$_POST['model_name'];
-    $model_num=$_POST['model_num'];
-    $ram=$_POST['ram'];
-    $gpu=$_POST['gpu'];
-    $os=$_POST['os'];
-    $processor_type=$_POST['processor_type'];
-    $processor_core=$_POST['processor_core'];
-    $internal_storage=$_POST['internal_storage'];
-    $battery=$_POST['battery'];
-
-    $fileExt=explode(".",$_FILES['photo']['name']);
-    $fileExt=strtolower(end($fileExt));
-    
-    // $_FILES['photo']['name']=md5($photo['name']).".".$fileExt;
-
-    $photo_location="images/mobile/".md5($photo['name']).".".$fileExt;
-
-
-
-    $sql="INSERT INTO mobile (photo,price,brand,model_name,model_number,ram,gpu,os,processor_type,processor_core,internal_storage,battery) VALUES ( '$photo_location','$price', '$brand', '$model_name', '$model_num', '$ram', '$gpu', '$os', '$processor_type', '$processor_core', '$internal_storage', '$battery') ;";
+    $sql="INSERT INTO camera (photo,price,brand,model_name,model_num,megapixel,sensor_type,type) VALUES ( '$photo_location','$price', '$brand', '$model_name', '$model_num', '$megapixel','$sensor_type','$type') ;";
 
     if(mysqli_query($conn,$sql))
     {
@@ -176,31 +134,27 @@ else if($device=='mobile')
 
 
 }
-if($device=='mobile')
+else if($device=='speaker')
 {
     $photo=$_FILES['photo'];
     $price=$_POST['price'];
     $brand=$_POST['brand'];
     $model_name=$_POST['model_name'];
     $model_num=$_POST['model_num'];
-    $ram=$_POST['ram'];
-    $gpu=$_POST['gpu'];
-    $os=$_POST['os'];
-    $processor_type=$_POST['processor_type'];
-    $processor_core=$_POST['processor_core'];
-    $internal_storage=$_POST['internal_storage'];
-    $battery=$_POST['battery'];
+    $type=$_POST['type'];
+    $wired=$_POST['wired'];
+    
 
     $fileExt=explode(".",$_FILES['photo']['name']);
     $fileExt=strtolower(end($fileExt));
     
     // $_FILES['photo']['name']=md5($photo['name']).".".$fileExt;
 
-    $photo_location="images/mobile/".md5($photo['name']).".".$fileExt;
+    $photo_location="images/speaker/".md5($photo['name']).".".$fileExt;
 
 
 
-    $sql="INSERT INTO mobile (photo,price,brand,model_name,model_number,ram,gpu,os,processor_type,processor_core,internal_storage,battery) VALUES ( '$photo_location','$price', '$brand', '$model_name', '$model_num', '$ram', '$gpu', '$os', '$processor_type', '$processor_core', '$internal_storage', '$battery') ;";
+    $sql="INSERT INTO speaker (photo,price,brand,model_name,model_number,type,wired) VALUES ( '$photo_location','$price', '$brand', '$model_name', '$model_num', '$type','$wired') ;";
 
     if(mysqli_query($conn,$sql))
     {
